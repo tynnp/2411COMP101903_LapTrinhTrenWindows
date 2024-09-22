137a9cd7e2f8c0d63047fe88ffe49182bf3bda9f
tynnp <148174474+tynnp@users.noreply.github.com>
2024-09-22 16:59:54 +0700

    thực hành 1 - bài 2.2

---
+++ Thuc_hanh/TH1_B2/TH1_B2_2.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace TH1_B2_2 {
+    class Program {
+        static void Main(string[] args) {
+            int n = int.Parse(Console.ReadLine());
+            Console.Write((int) Math.Sqrt(n) == Math.Sqrt(n));
+        }
+    }
+}
