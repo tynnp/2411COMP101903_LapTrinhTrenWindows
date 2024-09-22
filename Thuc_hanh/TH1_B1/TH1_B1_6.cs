789e69512ac220f4e5cd4859d58b2673c109e9c5
tynnp <148174474+tynnp@users.noreply.github.com>
2024-09-22 16:07:13 +0700

    thực hành 1 - bài 1.6

---
+++ Thuc_hanh/TH1_B1/TH1_B1_6.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace TH1_B1_6 {
+    class Program {
+        static void Main(string[] args) {
+            string n = Console.ReadLine();
+            Console.Write("{3}{2}{1}{0}", n[0], n[1], n[2], n[3]);
+        }
+    }
+}
