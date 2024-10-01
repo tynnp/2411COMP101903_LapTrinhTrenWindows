7b5eeb084eeee6b16e3e104a667243444acefd3d
Nguyễn Ngọc Phú Tỷ <148174474+tynnp@users.noreply.github.com>
2024-10-01 10:21:07 +0700

    lab 2 bài tập 1

--- Lab/Lab_2_List_Exception/Lab_2_BT1.cs
+++ Lab/Lab_2_List_Exception/Lab_2_BT1.cs
@@ -5,17 +5,15 @@ public class Program {
     public static void Main(string[] args) {
         Console.InputEncoding = Encoding.UTF8;
         Console.OutputEncoding = Encoding.UTF8;
-
         double x, y;
-        Console.Write("Nhập số x: ");
-        while (!double.TryParse(Console.ReadLine(), out x)) 
-            Console.Write("Nhập số x: ");
-        
-        Console.Write("Nhập số y: ");
-        while (!double.TryParse(Console.ReadLine(), out y))
-            Console.Write("Nhập số y: ");
 
         try {
+            Console.Write("Nhập số x: ");
+            x = double.Parse(Console.ReadLine());
+
+            Console.Write("Nhập số y: ");
+            y = double.Parse(Console.ReadLine());
+
             double H = Math.Sqrt((3*x + 2*y)/(2*x - y));
 
             if (2*x - y == 0) 
@@ -27,19 +25,19 @@ public class Program {
             Console.Write($"Kết quả = {H:0.00}");
 
         } catch (DivideByZeroException e) {
-            Console.WriteLine(e);
+            Console.WriteLine($"Error: {e.Message}");
         } catch (FormatException e) {
-            Console.WriteLine(e);
+            Console.WriteLine($"Error: {e.Message}");
         } catch (NotNegativeException e) {
-            Console.WriteLine(e);
+            Console.WriteLine($"Error: {e.Message}");
         } catch (Exception e) {
-            Console.WriteLine(e);
+            Console.WriteLine($"Error: {e.Message}");
         }
     }
 
     class NotNegativeException : Exception {
         public NotNegativeException() : base ("Gia tri nho hon 0!") {
-            Console.WriteLine("TEST BUG");
+            //
         }
     }
 }
