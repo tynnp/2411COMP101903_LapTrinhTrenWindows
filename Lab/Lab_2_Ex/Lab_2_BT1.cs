35d0338b2b0158b4720012343756946267770768
Nguyễn Ngọc Phú Tỷ <148174474+tynnp@users.noreply.github.com>
2024-10-02 15:36:28 +0700

    lab 2 bài tập 1

---
+++ Lab/Lab_2_Ex/Lab_2_BT1.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Text;
+
+public class Program {
+    public static void Main(string[] args) {
+        Console.InputEncoding = Encoding.UTF8;
+        Console.OutputEncoding = Encoding.UTF8;
+
+        int index;
+        string tmp = "";
+        List<string> lst = new List<string>();
+
+        while (true) {
+            Console.Write("Nhập tên hàng hóa: ");
+            tmp = Console.ReadLine().Trim();
+            if (tmp.ToUpper() == "STOP") break;
+            lst.Add(tmp);
+        }
+
+        Console.WriteLine("\nDanh sách hàng hóa: ");
+        foreach (string str in lst) 
+            Console.WriteLine(str);
+        Console.WriteLine();
+        
+        tmp = "";
+        while (string.IsNullOrWhiteSpace(tmp)) {
+            Console.Write("Nhập vào tên hàng hóa cần xóa: ");
+            tmp = Console.ReadLine().Trim(); 
+        }
+        
+        if (lst.Contains(tmp)) {
+            lst.Remove(tmp);
+            Console.WriteLine("Đã xóa!");
+        } else 
+            Console.WriteLine("Không tìm thấy!");
+
+        Console.Write("Nhập vị trí cần thêm: ");
+        while (!int.TryParse(Console.ReadLine(), out index))
+            Console.Write("Nhập vị trí cần thêm: ");
+
+        tmp = "";
+        while (string.IsNullOrWhiteSpace(tmp)) {
+            Console.Write("Nhập tên hàng hóa muốn thêm: ");
+            tmp = Console.ReadLine();
+        }
+
+        try {
+            lst.Insert(index, tmp);
+        } catch (Exception e) {
+            Console.Write(e.Message);
+        }
+
+        Console.WriteLine("\nDanh sách hàng hóa: ");
+        foreach (string str in lst) 
+            Console.WriteLine(str);
+        Console.WriteLine();
+
+        Console.WriteLine("Kết thúc chương trình!");
+    }
+}
