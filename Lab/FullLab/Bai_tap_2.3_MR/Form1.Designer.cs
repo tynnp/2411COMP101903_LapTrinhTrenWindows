bbe748fc28d5b23f30e613b314cc53f6a510eea4
Nguyễn Ngọc Phú Tỷ <148174474+tynnp@users.noreply.github.com>
2024-10-11 14:20:55 +0700

    cập nhật lại màu cho bài 2.3 - fulllab

--- Lab/FullLab/Bai_tap_2.3_MR/Form1.Designer.cs
+++ Lab/FullLab/Bai_tap_2.3_MR/Form1.Designer.cs
@@ -70,6 +70,7 @@
             // txtKetQua
             // 
             this.txtKetQua.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtKetQua.ForeColor = System.Drawing.Color.Red;
             this.txtKetQua.Location = new System.Drawing.Point(6, 33);
             this.txtKetQua.Multiline = true;
             this.txtKetQua.Name = "txtKetQua";
